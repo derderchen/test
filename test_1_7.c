8d8c32ffd8e20c77e81371e31cdea975936fc55a
derderchen <76468974+derderchen@users.noreply.github.com>
2021-01-07 20:05:50 +0800

    Add files via upload

---
+++ test_1_7.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+
+
+
+//int main()
+//{
+//	long int n, i;
+//	int h = 0;
+//	scanf("%d", &n);
+//	if (n < 12)
+//	{
+//		printf("2");
+//		return 0;
+//	}
+//	else
+//	{
+//		while (n/12!=0)
+//		{
+//			n -= 12;
+//			h += 4;
+//		}
+//		printf("%d", h+2);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int h, m, min,nm;
+//	scanf("%d:%d %d", &h, &m,&min);
+//	nm = m + min;
+//	if (nm / 60 != 0)
+//	{
+//		m = nm % 60;
+//		while (nm / 60)
+//		{
+//			nm = nm - 60;
+//			if (h == 23)
+//			{
+//				h = 0;
+//			}
+//			else
+//			h++;
+//		}
+//	}
+//	else
+//	{
+//		m = nm;
+//	}
+//	printf("%02d:%02d", h, m);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	long n = 0;
+//	int i = 1;
+//	unsigned long sum = 0;
+//	scanf("%ld", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		sum += i;
+//	}
+//	printf("%u", sum);
+//	return 0;
+//}
+
+
